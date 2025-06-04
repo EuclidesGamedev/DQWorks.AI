f71130a57e85f3f13b8253445405192ebc24d4de
EuclidesGamedev <193931530+EuclidesGamedev@users.noreply.github.com>
2025-06-04 02:26:29 -0300

    updated Navmesh2D class structure and added Grid property

--- Assets/Scripts/AI/Pathfinding/Navmesh2D.cs
+++ Assets/Scripts/AI/Pathfinding/Navmesh2D.cs
@@ -5,22 +5,34 @@ namespace Assets.Scripts.AI.Pathfinding
 {
     public class Navmesh2D : MonoBehaviour
     {
-        #region Getters and setters]
-        [field: Header("Grid settings")]
-        [field: SerializeField] public Vector2Int GridSize { get; private set; }
-        [field: SerializeField] public Vector2 NodeSize { get; private set; }
-        public Vector2 WorldPosition => transform.position;
+        #region Serialized fields
+        [Header("Navmesh settings")]
+        [SerializeField] private Vector2Int _gridSize = new Vector2Int(2, 2);
+        [SerializeField] private Vector2 _nodeSize = Vector2.one;
+        #endregion
+
+        #region Getters and setters
+        public Node2D[,] Grid { get; private set; } = { };
+        public Vector2Int GridSize => _gridSize;
+        public Vector2 NodeSize => _nodeSize;
+        public Vector2 Position => transform.position;
         public Vector2 WorldSize => GridSize * NodeSize;
         #endregion
 
         #region MonoBehaviour
+        private void Start()
+        {
+            GenerateGrid();
+        }
+
         private void Update()
         {
             if (InputSystem.actions.FindAction("UI/Click").WasPressedThisFrame())
             {
                 Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.value);
+                Node2D? node = GetNodeByWorldPosition(ray.origin);
 
-                Debug.Log("GridPosition is: " + WorldToGridPosition(ray.origin));
+                Debug.Log("GridPosition is: " + (node.HasValue ? node.Value.GridPosition : "null"));
             }
         }
 
@@ -31,30 +43,35 @@ namespace Assets.Scripts.AI.Pathfinding
         #endregion
 
         #region Navmesh2D private methods
-        private void RenderNavmesh()
-        { 
+        private void GenerateGrid()
+        {
+            Grid = new Node2D[GridSize.x, GridSize.y];
+
             for (int x = 0; x < GridSize.x; x++)
-            for (int y = 0; y < GridSize.y; y++)
-            {
-                    Vector2 position = GridToWorldPosition(
-                        Vector2Int.right * x + Vector2Int.up * y
-                    );
-                        
-                    Gizmos.color = Color.green;
-                    Gizmos.DrawWireCube(
-                        position, NodeSize
-                    );
-            }
+                for (int y = 0; y < GridSize.y; y++)
+                    Grid[x, y] = new Node2D(x, y);
+        }
+
+        private void RenderNavmesh()
+        {
+            Gizmos.color = Color.green;
+            foreach (Node2D node in Grid)
+                Gizmos.DrawWireCube(GridToWorldPosition(node.GridPosition), NodeSize);
         }
         #endregion
 
         #region Navmesh2D public methods
         public Node2D[] GetNeighbors(Node2D node) { throw new System.NotImplementedException(); }
-        public Node2D GetNodeByGridPosition(Vector2Int gridPosition) { throw new System.NotImplementedException(); }
-        public Node2D GetNodeByWorldPosition(Vector2 worldPosition) { throw new System.NotImplementedException(); }
+        public Node2D? GetNodeByGridPosition(Vector2Int gridPosition)
+        {
+            if (gridPosition.x < 0 || gridPosition.x >= GridSize.x) return null;
+            if (gridPosition.y < 0 || gridPosition.y >= GridSize.y) return null;
+            return Grid[gridPosition.x, gridPosition.y];
+        }
+        public Node2D? GetNodeByWorldPosition(Vector2 worldPosition) => GetNodeByGridPosition(WorldToGridPosition(worldPosition));
 
-        public Vector2 GridToWorldPosition(Vector2Int gridPosition) => WorldPosition + NodeSize * gridPosition - WorldSize / 2 + NodeSize / 2;
-        public Vector2Int WorldToGridPosition(Vector2 worldPosition) => Vector2Int.CeilToInt((worldPosition + WorldSize / 2 - WorldPosition - NodeSize) / NodeSize);
+        public Vector2 GridToWorldPosition(Vector2Int gridPosition) => Position + NodeSize * gridPosition - WorldSize / 2 + NodeSize / 2;
+        public Vector2Int WorldToGridPosition(Vector2 worldPosition) => Vector2Int.CeilToInt((worldPosition + WorldSize / 2 - Position - NodeSize) / NodeSize);
         #endregion
     }
 }
