0d48e25dabc64922f41885b3cd25d73c4672002b
EuclidesGamedev <193931530+EuclidesGamedev@users.noreply.github.com>
2025-06-05 13:12:49 -0300

    implemented Heuristics.CalculateH

--- Assets/Scripts/AI/Pathfinding/AStar/Heuristics.cs
+++ Assets/Scripts/AI/Pathfinding/AStar/Heuristics.cs
@@ -5,8 +5,10 @@ namespace Assets.Scripts.AI.Pathfinding.AStar
     public static class Heuristics
     {
         public static int CalculateF() { throw new System.NotImplementedException(); }
-        public static int CalculateG() { throw new System.NotImplementedException(); }
-        public static int CalculateH(Node2D node1, Node2D node2)
+        public static int CalculateG(PathNode node1, Node node2) {
+            return node1.CostG + CalculateH(node1.CurrentNode, node2);
+        }
+        public static int CalculateH(Node node1, Node node2)
         {
             Vector2Int diffVector = new Vector2Int(
                 Mathf.Abs(node1.GridPosition.x - node2.GridPosition.x),
