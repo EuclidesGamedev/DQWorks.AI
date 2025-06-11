f55e85fb3051a05eff6072d0c6b435879b775c8f
EuclidesGamedev <193931530+EuclidesGamedev@users.noreply.github.com>
2025-06-11 20:58:24 -0300

    created files of StateMachine's tests

---
+++ Assets/Scripts/Tests/AI/StateMachine/StateMachineTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using UnityEngine;
+
+namespace DQWorks.Tests.AI.StateMachine
+{
+    public class StateMachineTests
+    {
+        #region Setup for tests
+        [SetUp]
+        public void SetUp() { throw new System.NotImplementedException(); }
+        #endregion
+
+        #region Test cases
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Tests/AI/StateMachine/StatesForTests.cs
@@ -0,0 +1,36 @@
+using DQWorks.AI.StateMachine.Interfaces;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DQWorks.Tests.AI.StateMachine
+{
+    public class StateForTests : IState
+    {
+        public UnityAction OnStateEnter;
+        public UnityAction OnStateExit;
+
+        public void Enter() { OnStateEnter?.Invoke(); }
+        public void Exit() { OnStateExit?.Invoke(); }
+    }
+
+    public class SimpleStateForTests : ISimpleState
+    {
+        public UnityAction OnStateEnter;
+        public UnityAction OnStateExit;
+
+        public void Enter() { OnStateEnter?.Invoke(); }
+        public void Exit() { OnStateExit?.Invoke(); }
+        public void Update() { }
+    }
+
+    public class PhysicsStateForTests : IPhysicsState
+    {
+        public UnityAction OnStateEnter;
+        public UnityAction OnStateExit;
+
+        public void Enter() { OnStateEnter?.Invoke(); }
+        public void Exit() { OnStateExit?.Invoke(); }
+        public void FixedUpdate() { }
+        public void Update() { }
+    }
+}
