e4b3c9b411cfba4c92aad3ea66fe695f466a5ad5
EuclidesGamedev <193931530+EuclidesGamedev@users.noreply.github.com>
2025-06-11 21:08:36 -0300

    added not implemented methods represents tests of StateMachine into StateMachineTests class

--- Assets/Scripts/Tests/AI/StateMachine/StateMachineTests.cs
+++ Assets/Scripts/Tests/AI/StateMachine/StateMachineTests.cs
@@ -5,12 +5,34 @@ namespace DQWorks.Tests.AI.StateMachine
 {
     public class StateMachineTests
     {
+        private DQWorks.AI.StateMachine.StateMachine _stateMachine;
+        private StateForTests _state1, _state2;
+
         #region Setup for tests
         [SetUp]
-        public void SetUp() { throw new System.NotImplementedException(); }
+        public void SetUp()
+        {
+            _state1 = new StateForTests();
+            _state2 = new StateForTests();
+            _stateMachine = new();
+        }
         #endregion
 
         #region Test cases
+        [TestCase] public void TestEqualityOfStateInstances()
+        {
+            Assert.AreNotEqual(_state1, _state2);
+            Assert.AreEqual(_state1, _state1);
+            Assert.AreEqual(_state2, _state2);
+        }
+
+        public void TestAssertInitializeMethodInvokesStateEnter() { }
+        public void TestAssertStateMachinesInitialStateIsNull() { }
+        public void TestAssertTransitionMethodInvokesStateEnter() { }
+        public void TestAssertTransitionMethodInvokesStateExit() { }
+        public void TestCantInitializeNull() { }
+        public void TestCanTransitionToCurrentState() { }
+        public void TestCantTransitionToNull() { }
         #endregion
     }
 }
